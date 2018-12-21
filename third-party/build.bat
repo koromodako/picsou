dc881dc099c33a0f31de4841e841c66f8d182645
koromodako <koromodako@gmail.com>
2018-12-21 08:41:42 -0800

    Update build.bat

--- third-party/build.bat
+++ third-party/build.bat
@@ -1,8 +1,10 @@
 #
 # Variables
 #
-set build=%cd%\build
-set botan=%cd%\botan-2.8.0
+set pwd=%cd%
+set build=%pwd%\build
+set botan=%pwd%\botan-2.8.0
+set picbuild=%pwd%\..\picsou\third-party\build
 #
 # Script
 #
@@ -12,4 +14,6 @@ cd %botan%
 python.exe configure.py --cc=gcc --os=mingw --prefix=%build%
 mingw32-make.exe
 mingw32-make.exe install
-cd ..
+cd %pwd%
+mkdir %picbuild%
+xcopy /s/e %build% %picbuild%
