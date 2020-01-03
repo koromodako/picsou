89db2fa492e148bde22834baef444c8f4f630389
koromodako <koromodako@gmail.com>
2020-01-03 12:19:53 +0100

    Update and rename build.bat to build.ps1

---
+++ third-party/build.ps1
@@ -0,0 +1,15 @@
+#
+# Variables
+#
+$refd=$pwd.Path
+$botan="$($refd)\botan-2.12.1"
+$distdir=(Resolve-Path "$($refd)\..\picsou\third-party\build").Path
+#
+# Script
+#
+mkdir $distdir
+cd $botan
+python.exe configure.py --cc=gcc --os=mingw --prefix=$distdir
+mingw32-make.exe -j4
+mingw32-make.exe install
+cd $refd
